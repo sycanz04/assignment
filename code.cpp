66010d6f1c0d1103c46a954c01f61d418ff01ee1
Aiden Chan <119921047+sycanz04@users.noreply.github.com>
2023-12-30 17:44:32 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -22,7 +22,7 @@ int main () {
     // Use a while loop together with the getline() function to read the file line by line
     while (getline (MyReadFile, myText)) {
         // Output the text from the file
-        cout << myText;
+        cout << myText << endl;
     }
 
     // Close the file
