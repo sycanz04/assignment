35e52503736f95bbdc3634329923aa7e35bd98a0
Aiden Chan <aidenchan0397@gmail.com>
2024-01-26 16:18:33 +0800

    update

--- try.cpp
+++ try.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include <string> 
 #include <iomanip>
 using namespace std;
 
+//Memory and register sizes
 const int MEM_SIZE = 8;
 const int REG_SIZE = 8;
 
-int memory[MEM_SIZE][MEM_SIZE] = {0}; // Initializing memory with zeros
+//Initialising 2D and 1D array for memory and registers respectively
+int memory[MEM_SIZE] = {0}; // Initializing memory with zeros
 int registers[REG_SIZE] = {0}; // Initializing registers with zeros
+
+//Function prototypes
+void reading();
+void MemAndReg();
 
 void MemAndReg() {
 
@@ -17,25 +25,71 @@ void MemAndReg() {
     }
     cout << endl << endl;
 
-        // Print Memory
+    // Print Memory
     cout << "Memory:" << endl;
     for (int i = 0; i < MEM_SIZE; ++i) {
         for (int j = 0; j < MEM_SIZE; ++j) {
-            cout << setw(4) << memory[i][j] << " ";
+            cout << setw(4) << memory[i] << " ";
         }
         cout << endl;
     }
 }
 
 
-int main() {
-    // Example: Load values into memory and registers
-    memory[0][0] = 10;
-    memory[1][1] = 20;
-    registers[0] = 5;
-    registers[1] = 15;
+//Read file function
+void reading(){
+    // Check if the file "data.txt" exists
+    ifstream check("data.asm");
+    bool exists = check.good();
+    check.close();
 
-    // Print initial state
+    // If it doesn't exists, create a new text file
+    if (!exists){
+        ofstream datafile("data.asm");
+    }
+
+    else{
+        // Create a text string, which is used to output the text file
+        string line;
+        const string move = "MOV";
+        const string add = "ADD";
+        const string mul = "MUL";
+        const string store = "STORE";
+
+        // Read from the text file
+        ifstream datafile("data.asm");
+
+        int pc = 0;
+        // Use loop to read file line by line
+        while (getline (datafile, line)) {
+            // If found keyword in string, then execute command
+            if (line.find(move) != string::npos) {
+                cout << "MOV found" << endl;
+                pc += 1;
+            }
+            else if (line.find(add) != string::npos) {
+                cout << "ADD found" << endl; 
+                pc += 1;
+            }
+            else if (line.find(mul) != string::npos) {
+                cout << "MUL found" << endl;
+                pc += 1;
+            }
+            else if (line.find(store) != string::npos) {
+                cout << "STORE found" << endl;
+                pc += 1;
+            }
+            else {
+                cout << "No commands found" << endl;
+            }
+        }
+        cout << "Program Counter: " << pc << endl;
+    }
+}
+
+int main()
+{
+    reading();
     MemAndReg();
 
     return 0;
